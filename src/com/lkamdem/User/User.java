61900b89a5d6b56add2ecd747f72882ca0d500a7
Leo70001 <lkamdem29@gmail.com>
2026-04-27 21:50:42 +0200

    sequence diagram

---
+++ src/com/lkamdem/User/User.java
@@ -0,0 +1,9 @@
+package com.lkamdem.User;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+
+}
---
+++ src/com/lkamdem/account/Account.java
@@ -0,0 +1,8 @@
+package com.lkamdem.account;
+
+public class Account {
+    private int accountNumber;
+    private double balance;
+    private int user_id;
+
+}
